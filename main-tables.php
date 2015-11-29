8d69f17a8702f25e6a690949fe45d24983869a58
owolcsics <oreste1907@yahoo.com>
2015-11-29 04:40:03 +0000

    Bla

--- main-tables.php
+++ main-tables.php
@@ -181,10 +181,10 @@ if(isset($_POST['logout'])){
         </div>
     </section>
 
-    <section>
+    <section id = "tabel">
     	  <?php
     		if($search_data){
-    			echo '<table class = "CSS_Table_Example">';
+    			echo '<table>';
     			echo '<tr><th>Amount in pounds</th><th>Interest rate</th><th>Number of days</th></tr>' ;
     			while($row = $search_data->fetch_assoc()){
     			echo '<tr>';
@@ -198,6 +198,39 @@ if(isset($_POST['logout'])){
 
 	?>
     </section>
+
+ <section>
+    <div class="container">
+    <h2>Striped Rows</h2>
+      <p>The .table-striped class adds zebra-stripes to a table:</p>            
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr>
+          <tr>
+            <td>Mary</td>
+            <td>Moe</td>
+            <td>mary@example.com</td>
+          </tr>
+          <tr>
+            <td>July</td>
+            <td>Dooley</td>
+            <td>july@example.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</section>
 
     <section id="lend">
         <div class="container">
